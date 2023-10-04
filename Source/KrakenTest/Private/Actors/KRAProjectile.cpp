d713907c59f94a2ecb0ec1e59c53d877247095b6
Gonza Ferretti <gonzalofe1998@hotmail.com>
2023-10-04 03:00:19 -0300

    mini cleanup

--- Source/KrakenTest/Private/Actors/KRAProjectile.cpp
+++ Source/KrakenTest/Private/Actors/KRAProjectile.cpp
@@ -20,6 +20,13 @@ AKRAProjectile::AKRAProjectile()
 	Collision->SetupAttachment(RootComponent);
 }
 
+void AKRAProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Collision->OnComponentBeginOverlap.AddUniqueDynamic(this, &AKRAProjectile::OnActorOverlapped);
+}
+
 void AKRAProjectile::OnActorOverlapped(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Overlap"));
@@ -32,11 +39,4 @@ void AKRAProjectile::OnActorOverlapped(UPrimitiveComponent* OverlappedComponent,
 	}
 
 	Destroy();
-}
-
-void AKRAProjectile::BeginPlay()
-{
-	Super::BeginPlay();
-
-	Collision->OnComponentBeginOverlap.AddUniqueDynamic(this, &AKRAProjectile::OnActorOverlapped);
 }
